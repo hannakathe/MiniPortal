1b0bb5a21f49927bb7136608f502213381b02cbc
Hannah K. Abril <95305822+hannakathe@users.noreply.github.com>
2025-08-31 15:42:07 -0500

    Add SQLAlchemy models for Song entity in the database

--- mini_portal_musica/backend/app/models.py
+++ mini_portal_musica/backend/app/models.py
@@ -2,3 +2,17 @@
 
 """ Database models using SQLAlchemy ORM.
 Aquí defines cómo se guardan las tablas."""
+
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class Song(Base):
+    __tablename__ = "songs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=False)
+    artist = Column(String, index=True, nullable=False)
+    album = Column(String, nullable=True)
+    year = Column(Integer, nullable=True)
+    genre = Column(String, nullable=True)
+
